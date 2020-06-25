0aac2d9b5a40d82f0bd55afcf9fcbebea5e52318
Abigail Chin <abby@tecarta.com>
2020-06-25 14:42:40 -0700

    save preferred highlight sheet--large or small

--- lib/models/labels.dart
+++ lib/models/labels.dart
@@ -2,6 +2,11 @@ class Labels {
   static const appNameForUA = 'tecarta_bible';
 
   // Preference key names:
+
+  // Font 
   static const prefContentTextScaleFactor = 'contentTextScaleFactor';
   static const prefContentFontName = 'contentFontName';
+
+  // Highlighting
+  static const prefSelectionSheetFullSize = 'selectionSheetFullSize';
 }
