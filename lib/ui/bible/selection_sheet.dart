e5977788cc34d55c4e58b58f6e06e668ca85d5ac
Abigail Chin <abby@tecarta.com>
2020-06-29 12:16:31 -0700

    selection sheet update to include color slider

--- lib/ui/bible/selection_sheet.dart
+++ lib/ui/bible/selection_sheet.dart
@@ -4,8 +4,8 @@ import 'package:feather_icons_flutter/feather_icons_flutter.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tec_util/tec_util.dart' as tec;
 
+import '../../blocs/app_theme_bloc.dart';
 import '../../blocs/selection/selection_bloc.dart';
-import '../../models/labels.dart';
 
 class SelectionSheet extends StatefulWidget {
   @override
@@ -13,24 +13,9 @@ class SelectionSheet extends StatefulWidget {
 }
 
 class _SelectionSheetState extends State<SelectionSheet> {
-  final colors = [
-    Colors.pink,
-    Colors.deepOrange,
-    Colors.amber,
-    Colors.green,
-    Colors.cyan,
-    Colors.indigo
-  ];
-
-  final primaries = Colors.primaries +
-      Colors.primaries +
-      Colors.primaries +
-      Colors.primaries +
-      Colors.primaries +
-      Colors.primaries;
+  List<Color> mainColors = [];
 
   final icons = [
-    FeatherIcons.underline,
     FeatherIcons.edit,
     FeatherIcons.share,
     FeatherIcons.copy,
@@ -48,52 +33,39 @@ class _SelectionSheetState extends State<SelectionSheet> {
     // 'Compare': FeatherIcons.compass
   };
 
-  bool _isFullSized;
   bool _showAllColors;
+  bool _underlineMode;
 
   @override
   void initState() {
-    _isFullSized = tec.Prefs.shared
-        .getBool(Labels.prefSelectionSheetFullSize, defaultValue: false);
+    for (var i = 1; i < 5; i++) {
+      mainColors.add(tec.colorFromColorId(i,
+          darkMode: context.bloc<ThemeModeBloc>().state == ThemeMode.dark));
+    }
+
+    // _isFullSized = tec.Prefs.shared
+    //     .getBool(Labels.prefSelectionSheetFullSize, defaultValue: false);
+
     _showAllColors = false;
+    _underlineMode = false;
     super.initState();
   }
-
-  // void onExpanded() {
-  //   setState(() {
-  //     _isFullSized = !_isFullSized;
-  //     tec.Prefs.shared.setBool(Labels.prefSelectionSheetFullSize, _isFullSized);
-  //     // if (!_isFullSized && _showAllColors) {
-  //     //   _showAllColors = !_showAllColors;
-  //     // }
-  //   });
-  // }
 
   void onShowAllColors() {
     setState(() {
       _showAllColors = !_showAllColors;
-      if (_showAllColors && !_isFullSized || !_showAllColors && _isFullSized) {
-        _isFullSized = !_isFullSized;
-      }
+    });
+  }
+
+  void onSwitchToUnderline() {
+    setState(() {
+      _underlineMode = !_underlineMode;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // var _colors = List<Color>.from(colors);
-    // // var _icons = List<IconData>.from(icons);
-    // if (!_isFullSized) {
-    //   _colors = _colors.take(3).toList();
-    //   // _icons = _icons.take(3).toList();
-    // }
-
-    const vDiv = VerticalDivider(
-      color: Colors.transparent,
-      width: 10,
-    );
-
-    final sheetChildren = [
-      // _ClearHighlightButton(context: context),
+    final clearAndUnderline = [
       _GreyCircleButton(
         icon: Icons.format_color_reset,
         onPressed: () =>
@@ -101,20 +73,44 @@ class _SelectionSheetState extends State<SelectionSheet> {
                   type: HighlightType.clear,
                 )),
       ),
-      for (final color in colors) ...[
-        _ColorPickerButton(
-          context: context,
-          color: color,
-        ),
+      _GreyCircleButton(
+        icon: _underlineMode ? FeatherIcons.edit3 : FeatherIcons.underline,
+        onPressed: onSwitchToUnderline,
+      )
+    ];
+
+    final sheetChildren = [
+      if (!_showAllColors) ...[
+        ...clearAndUnderline,
+        for (final color in mainColors) ...[
+          _ColorPickerButton(
+            isForUnderline: _underlineMode,
+            color: color,
+          ),
+        ],
+      ] else ...[
+        _ColorSlider(isUnderline: _underlineMode),
+        ...clearAndUnderline
       ],
-      // _GreyCircleButton(
-      //   icon: Icons.color_lens,
-      //   onPressed: onShowAllColors,
-      // ),
-      // vDiv,
+      _GreyCircleButton(
+        icon: Icons.color_lens,
+        onPressed: onShowAllColors,
+      ),
       for (final each in icons) ...[
         _GreyCircleButton(icon: each, onPressed: () {}),
       ],
+      const Divider(color: Colors.transparent),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          for (final each in buttons.keys) ...[
+            Expanded(
+              child: _SheetButton(text: each, icon: buttons[each]),
+            ),
+            if (buttons.keys.last != each) const SizedBox(width: 10)
+          ]
+        ],
+      ),
     ];
 
     Widget sheet() => Padding(
@@ -135,55 +131,11 @@ class _SelectionSheetState extends State<SelectionSheet> {
         );
 
     return _RoundedCornerSheet(
-      isFullSized: _isFullSized,
       showAllColors: _showAllColors,
-      // onExpanded: onExpanded,
       child: Column(
         children: [
-          // if (_isFullSized && !_showAllColors) ...[
-          // ],
-          Expanded(
-              flex: 2,
-              child: _showAllColors
-                  ? Padding(
-                      padding: const EdgeInsets.only(left: 15, right: 15),
-                      child: Row(children: [
-                        Expanded(
-                          child: Wrap(
-                            children: [
-                              for (final each in primaries)
-                                SizedBox(
-                                  height: 20,
-                                  width: 20,
-                                  child: Container(
-                                    color: each,
-                                  ),
-                                ),
-                            ],
-                          ),
-                        ),
-                        _GreyCircleButton(
-                          icon: Icons.close,
-                          onPressed: onShowAllColors,
-                        ),
-                      ]),
-                    )
-                  : sheet()),
-          Expanded(
-              child: Padding(
-            padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                for (final each in buttons.keys) ...[
-                  Expanded(
-                    child: _SheetButton(text: each, icon: buttons[each]),
-                  ),
-                  if (buttons.keys.last != each) const SizedBox(width: 10)
-                ]
-              ],
-            ),
-          )),
+          SheetAppBar(),
+          Expanded(flex: 2, child: sheet()),
           const Divider(
             color: Colors.transparent,
           ),
@@ -193,23 +145,156 @@ class _SelectionSheetState extends State<SelectionSheet> {
   }
 }
 
-class _ColorPickerButton extends StatelessWidget {
-  final BuildContext context;
-  final Color color;
-  const _ColorPickerButton({@required this.context, @required this.color})
-      : assert(context != null),
-        assert(color != null);
+class SheetAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 30,
+      child: PreferredSize(
+        preferredSize: const Size.fromHeight(30),
+        child: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          leading: Container(),
+          elevation: 0,
+          title: Container(
+            width: 50,
+            height: 5,
+            decoration: ShapeDecoration(
+                color: Theme.of(context)
+                    .textTheme
+                    .bodyText1
+                    .color
+                    .withOpacity(0.2),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15))),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ColorSlider extends StatefulWidget {
+  final bool isUnderline;
+  const _ColorSlider({this.isUnderline});
+  @override
+  __ColorSliderState createState() => __ColorSliderState();
+}
+
+class __ColorSliderState extends State<_ColorSlider> {
+  List<Color> allColors = [];
+  double _colorValue;
+  bool _canEnd;
+
+  @override
+  void initState() {
+    for (var i = 6; i < 365; i++) {
+      allColors.add(tec.colorFromColorId(i,
+          darkMode: context.bloc<ThemeModeBloc>().state == ThemeMode.dark));
+    }
+    _colorValue = 0;
+    super.initState();
+  }
+
+  void changeColor(double value) {
+    context.bloc<SelectionStyleBloc>()?.add(
+          SelectionStyle(
+              type: widget.isUnderline
+                  ? HighlightType.underline
+                  : HighlightType.highlight,
+              color: allColors[_colorValue.toInt()].value,
+              isTrialMode: true),
+        );
+    setState(() {
+      _colorValue = value;
+      _canEnd = true;
+    });
+  }
+
+  void finishWithColor(double value) {
+    if (_canEnd) {
+      context.bloc<SelectionStyleBloc>()?.add(
+            SelectionStyle(
+              type: widget.isUnderline
+                  ? HighlightType.underline
+                  : HighlightType.highlight,
+              color: allColors[_colorValue.toInt()].value,
+            ),
+          );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      customBorder: const CircleBorder(),
-      onTap: () => context.bloc<SelectionStyleBloc>()?.add(
-          SelectionStyle(type: HighlightType.highlight, color: color.value)),
-      child: CircleAvatar(
-        backgroundColor: color,
-      ),
+    return Padding(
+      padding: const EdgeInsets.only(left: 15, right: 15),
+      child: Stack(alignment: Alignment.center, children: [
+        Container(
+          height: 20,
+          decoration: ShapeDecoration(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              gradient: LinearGradient(colors: allColors)),
+        ),
+        GestureDetector(
+            onTapCancel: () {
+              _canEnd = false;
+            },
+            child: SliderTheme(
+                data: SliderThemeData(
+                    thumbColor: allColors[_colorValue.toInt()],
+                    activeTrackColor: Colors.transparent,
+                    inactiveTrackColor: Colors.transparent,
+                    activeTickMarkColor: Colors.green,
+                    thumbShape:
+                        const RoundSliderThumbShape(enabledThumbRadius: 20)),
+                child: Slider(
+                  onChanged: changeColor,
+                  onChangeEnd: finishWithColor,
+                  value: _colorValue,
+                  min: 0,
+                  max: 358,
+                ))),
+      ]),
     );
+  }
+}
+
+class _ColorPickerButton extends StatelessWidget {
+  final bool isForUnderline;
+  final Color color;
+  const _ColorPickerButton({@required this.color, this.isForUnderline = false})
+      : assert(color != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return isForUnderline
+        ? InkWell(
+            customBorder: const RoundedRectangleBorder(),
+            onTap: () => context.bloc<SelectionStyleBloc>()?.add(SelectionStyle(
+                  type: HighlightType.underline,
+                  color: color.value,
+                )),
+            child: Container(
+              decoration: ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  color: color),
+              width: 40,
+              height: 15,
+            ),
+          )
+        : InkWell(
+            customBorder: const CircleBorder(),
+            onTap: () => context.bloc<SelectionStyleBloc>()?.add(SelectionStyle(
+                  type: HighlightType.highlight,
+                  color: color.value,
+                )),
+            child: CircleAvatar(
+              backgroundColor: color,
+            ),
+          );
   }
 }
 
@@ -231,25 +316,6 @@ class _SheetButton extends StatelessWidget {
             size: 18,
           ),
           onPressed: () {}),
-    );
-  }
-}
-
-class _ClearHighlightButton extends StatelessWidget {
-  final BuildContext context;
-  const _ClearHighlightButton({@required this.context})
-      : assert(context != null);
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      customBorder: const CircleBorder(),
-      onTap: () => context.bloc<SelectionStyleBloc>()?.add(const SelectionStyle(
-            type: HighlightType.clear,
-          )),
-      child: ClipOval(
-          child: CustomPaint(
-              painter: DiagonalLinePainter(),
-              child: const _GreyCircleButton())),
     );
   }
 }
@@ -285,39 +351,21 @@ class _GreyCircleButton extends StatelessWidget {
 
 class _RoundedCornerSheet extends StatelessWidget {
   final Widget child;
-  final bool isFullSized;
   final bool showAllColors;
-  // final VoidCallback onExpanded;
+
   const _RoundedCornerSheet({
     @required this.child,
-    this.isFullSized = true,
     this.showAllColors = false,
-    // @required this.onExpanded
   }) : assert(child != null);
   @override
   Widget build(BuildContext context) {
     return DraggableScrollableSheet(
-        //   child: GestureDetector(
-        // onVerticalDragEnd: (details) {
-        //   final dy = details.velocity.pixelsPerSecond.dy;
-        //   // debugPrint(dy.toString());
-        //   if (dy > 0) {
-        //     Navigator.of(context).pop();
-        //   } else if (isFullSized && dy >= 0 || !isFullSized && dy <= 0) {
-        //     onExpanded();
-        //   }
-        // },
-        initialChildSize: 0.11,
+        initialChildSize: 0.12,
         minChildSize: 0.11,
         maxChildSize: 0.3,
         expand: false,
         builder: (c, scrollController) {
           return Container(
-            // AnimatedContainer(
-            //   duration: const Duration(milliseconds: 300),
-            //   curve: Curves.fastOutSlowIn,
-            //   height: isFullSized ? 200 : 100,
-            // margin: const EdgeInsets.fromLTRB(100, 0, 100, 0),
             decoration: ShapeDecoration(
                 shadows: Theme.of(context).brightness == Brightness.light
                     ? [
@@ -335,56 +383,13 @@ class _RoundedCornerSheet extends StatelessWidget {
                   topRight: Radius.circular(15),
                   topLeft: Radius.circular(15),
                 ))),
-            child: Scaffold(
-                backgroundColor: Colors.transparent,
-                appBar: PreferredSize(
-                  preferredSize: const Size.fromHeight(30),
-                  child: AppBar(
-                    centerTitle: true,
-                    backgroundColor: Colors.transparent,
-                    leading: Container(),
-                    elevation: 0,
-                    title: Container(
-                      width: 50,
-                      height: 5,
-                      decoration: ShapeDecoration(
-                          color: Theme.of(context)
-                              .textTheme
-                              .bodyText1
-                              .color
-                              .withOpacity(0.2),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15))),
-                    ),
-                  ),
-                ),
-                body: SingleChildScrollView(
-                  controller: scrollController,
-                  child: SafeArea(child: Container(height: 200, child: child)),
-                )),
+            child: SingleChildScrollView(
+              controller: scrollController,
+              child: SafeArea(child: Container(height: 310, child: child)),
+            ),
           );
         }
         // ),
         );
-  }
-}
-
-class DiagonalLinePainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = Colors.red
-      ..strokeWidth = 5
-      ..strokeCap = StrokeCap.round;
-
-    final startingPoint = Offset(0, size.height);
-    final endingPoint = Offset(size.width, 0);
-
-    canvas.drawLine(startingPoint, endingPoint, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
   }
 }
