dbf5c05745fe194e47ff64e2ad98158ac47841f6
Abigail Chin <abby@tecarta.com>
2020-06-30 17:15:41 -0700

    sliding sheet that snaps into place with adjustment for screen size

--- lib/ui/bible/selection_sheet.dart
+++ lib/ui/bible/selection_sheet.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 
 import 'package:feather_icons_flutter/feather_icons_flutter.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sliding_sheet/sliding_sheet.dart';
 import 'package:tec_util/tec_util.dart' as tec;
+import 'package:tec_widgets/tec_widgets.dart';
 
 import '../../blocs/app_theme_bloc.dart';
 import '../../blocs/selection/selection_bloc.dart';
 
 class SelectionSheet extends StatefulWidget {
+  // final bool fullyExpanded;
+  // const SelectionSheet({this.fullyExpanded});
   @override
   _SelectionSheetState createState() => _SelectionSheetState();
 }
@@ -15,17 +19,22 @@ class SelectionSheet extends StatefulWidget {
 class _SelectionSheetState extends State<SelectionSheet> {
   List<Color> mainColors = [];
 
-  final icons = [
-    FeatherIcons.edit,
-    FeatherIcons.share,
-    FeatherIcons.copy,
-    FeatherIcons.bookmark,
-    Icons.lightbulb_outline,
-    Icons.compare_arrows,
-    FeatherIcons.compass
-  ];
-
   final buttons = <String, IconData>{
+    'Learn': Icons.lightbulb_outline,
+    'Explore': FeatherIcons.compass,
+    'Compare': Icons.compare_arrows,
+    'Define': FeatherIcons.bookOpen,
+    'Copy': FeatherIcons.copy,
+    'Notes': FeatherIcons.edit2,
+    'Save': FeatherIcons.bookmark,
+    'Print': FeatherIcons.printer,
+    'Text': FeatherIcons.messageCircle,
+    'Email': FeatherIcons.mail,
+    'Facebook': FeatherIcons.facebook,
+    'Twitter': FeatherIcons.twitter
+  };
+
+  final squareButtons = <String, IconData>{
     'Copy': FeatherIcons.copy,
     'Note': FeatherIcons.edit,
     'Share': FeatherIcons.share,
@@ -65,112 +74,98 @@ class _SelectionSheetState extends State<SelectionSheet> {
 
   @override
   Widget build(BuildContext context) {
-    final clearAndUnderline = [
-      _GreyCircleButton(
-        icon: Icons.format_color_reset,
-        onPressed: () =>
-            context.bloc<SelectionStyleBloc>()?.add(const SelectionStyle(
-                  type: HighlightType.clear,
-                )),
-      ),
-      _GreyCircleButton(
-        icon: _underlineMode ? FeatherIcons.edit3 : FeatherIcons.underline,
-        onPressed: onSwitchToUnderline,
-      )
-    ];
-
-    final sheetChildren = [
-      if (!_showAllColors) ...[
-        ...clearAndUnderline,
-        for (final color in mainColors) ...[
-          _ColorPickerButton(
-            isForUnderline: _underlineMode,
-            color: color,
-          ),
-        ],
-      ] else ...[
-        _ColorSlider(isUnderline: _underlineMode),
-        ...clearAndUnderline
-      ],
-      _GreyCircleButton(
-        icon: Icons.color_lens,
-        onPressed: onShowAllColors,
-      ),
-      for (final each in icons) ...[
-        _GreyCircleButton(icon: each, onPressed: () {}),
-      ],
-      const Divider(color: Colors.transparent),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          for (final each in buttons.keys) ...[
-            Expanded(
-              child: _SheetButton(text: each, icon: buttons[each]),
+    return Padding(
+      padding: const EdgeInsets.only(left: 15, right: 15),
+      child: Column(children: [
+        if (!_showAllColors)
+          Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+            _GreyCircleButton(
+              icon: Icons.format_color_reset,
+              onPressed: () =>
+                  context.bloc<SelectionStyleBloc>()?.add(const SelectionStyle(
+                        type: HighlightType.clear,
+                      )),
             ),
-            if (buttons.keys.last != each) const SizedBox(width: 10)
-          ]
-        ],
-      ),
-    ];
-
-    Widget sheet() => Padding(
-        padding: const EdgeInsets.only(left: 15, right: 15),
-        child: Wrap(
-          alignment: WrapAlignment.center,
-          crossAxisAlignment: WrapCrossAlignment.center,
-          runSpacing: 5,
-          spacing: 5,
-          children: sheetChildren,
-        )
-        // : ListView.separated(
-        //     scrollDirection: Axis.horizontal,
-        //     itemCount: sheetChildren.length,
-        //     itemBuilder: (c, i) => sheetChildren[i],
-        //     separatorBuilder: (c, i) => vDiv,
-        //   )
-        );
-
-    return _RoundedCornerSheet(
-      showAllColors: _showAllColors,
-      child: Column(
-        children: [
-          SheetAppBar(),
-          Expanded(flex: 2, child: sheet()),
-          const Divider(
-            color: Colors.transparent,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class SheetAppBar extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 30,
-      child: PreferredSize(
-        preferredSize: const Size.fromHeight(30),
-        child: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.transparent,
-          leading: Container(),
-          elevation: 0,
-          title: Container(
-            width: 50,
-            height: 5,
-            decoration: ShapeDecoration(
-                color: Theme.of(context)
-                    .textTheme
-                    .bodyText1
-                    .color
-                    .withOpacity(0.2),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15))),
-          ),
+            _GreyCircleButton(
+              icon:
+                  _underlineMode ? FeatherIcons.edit3 : FeatherIcons.underline,
+              onPressed: onSwitchToUnderline,
+            ),
+            for (final color in mainColors) ...[
+              _ColorPickerButton(
+                isForUnderline: _underlineMode,
+                color: color,
+              ),
+            ],
+            _GreyCircleButton(
+              icon: Icons.color_lens,
+              onPressed: onShowAllColors,
+            ),
+          ])
+        else
+          Row(children: [
+            Expanded(child: _ColorSlider(isUnderline: _underlineMode)),
+            _GreyCircleButton(
+              icon: Icons.format_color_reset,
+              onPressed: () =>
+                  context.bloc<SelectionStyleBloc>()?.add(const SelectionStyle(
+                        type: HighlightType.clear,
+                      )),
+            ),
+            const SizedBox(width: 5),
+            _GreyCircleButton(
+              icon:
+                  _underlineMode ? FeatherIcons.edit3 : FeatherIcons.underline,
+              onPressed: onSwitchToUnderline,
+            ),
+            const SizedBox(width: 5),
+            _GreyCircleButton(
+              icon: Icons.color_lens,
+              onPressed: onShowAllColors,
+            ),
+          ]),
+        const Divider(
+          endIndent: 15,
+          indent: 15,
         ),
-      ),
+        const Divider(
+          color: Colors.transparent,
+          height: 10,
+        ),
+        // if (!widget.fullyExpanded)
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            for (final each in squareButtons.keys) ...[
+              Expanded(
+                child: _SheetButton(text: each, icon: squareButtons[each]),
+              ),
+              if (buttons.keys.last != each) const SizedBox(width: 10)
+            ]
+          ],
+        ),
+        const Divider(color: Colors.transparent),
+        Container(
+          height: 300,
+          child: GridView.count(
+              crossAxisCount: 4,
+              padding: const EdgeInsets.only(top: 0),
+              physics: const NeverScrollableScrollPhysics(),
+              // alignment: WrapAlignment.spaceAround,
+              // crossAxisAlignment: WrapCrossAlignment.center,
+              // runSpacing: 10,
+              // spacing: 15,
+              children: [
+                for (final each in buttons.keys) ...[
+                  _GreyCircleButton(
+                    icon: buttons[each],
+                    onPressed: () {},
+                    title: each,
+                  ),
+                ],
+              ]),
+        ),
+      ]),
     );
   }
 }
@@ -323,73 +318,101 @@ class _SheetButton extends StatelessWidget {
 class _GreyCircleButton extends StatelessWidget {
   final IconData icon;
   final VoidCallback onPressed;
-  const _GreyCircleButton({this.icon, this.onPressed});
+  final String title;
+  const _GreyCircleButton(
+      {@required this.icon, @required this.onPressed, this.title});
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      customBorder: const CircleBorder(),
-      onTap: onPressed ?? () {},
-      child: Container(
-          child: CircleAvatar(
-              backgroundColor: Colors.transparent,
-              child: icon != null
-                  ? Icon(
-                      icon,
-                      color: Colors.grey,
-                    )
-                  : null),
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            border: Border.all(
-              color: Colors.grey.withOpacity(0.2),
-              width: 5,
-            ),
+    Widget circleIcon([double radius]) => Container(
+        child: InkWell(
+            customBorder: const CircleBorder(),
+            onTap: onPressed ?? () {},
+            child: CircleAvatar(
+                radius: radius,
+                backgroundColor: Colors.transparent,
+                child: icon != null
+                    ? Icon(
+                        icon,
+                        color: Colors.grey,
+                      )
+                    : null)),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: Colors.grey.withOpacity(0.2),
+            width: 5,
+          ),
+        ));
+    return Column(
+      children: [
+        if (title != null) ...[
+          Expanded(flex: 2, child: circleIcon(30)),
+          Expanded(
+              child: TecText(
+            title,
+            autoSize: true,
           )),
+        ] else
+          circleIcon(),
+      ],
     );
   }
 }
 
-class _RoundedCornerSheet extends StatelessWidget {
+class SnapSheet extends StatefulWidget {
+  final SheetController controller;
+  final Function(SheetState, double) onSnap;
+  final Widget body;
   final Widget child;
-  final bool showAllColors;
+  final List<double> snappings;
 
-  const _RoundedCornerSheet({
+  const SnapSheet({
+    @required this.controller,
     @required this.child,
-    this.showAllColors = false,
-  }) : assert(child != null);
+    @required this.onSnap,
+    @required this.snappings,
+    this.body,
+  })  : assert(controller != null),
+        assert(child != null),
+        assert(onSnap != null),
+        assert(snappings != null);
+
+  @override
+  _SnapSheetState createState() => _SnapSheetState();
+}
+
+class _SnapSheetState extends State<SnapSheet> {
   @override
   Widget build(BuildContext context) {
-    return DraggableScrollableSheet(
-        initialChildSize: 0.12,
-        minChildSize: 0.11,
-        maxChildSize: 0.3,
-        expand: false,
-        builder: (c, scrollController) {
+    return SlidingSheet(
+        controller: widget.controller,
+        elevation: 8,
+        cornerRadius: 15,
+        isBackdropInteractable: true,
+        duration: const Duration(milliseconds: 250),
+        addTopViewPaddingOnFullscreen: true,
+        snapSpec: SnapSpec(
+          snappings: widget.snappings,
+          onSnap: widget.onSnap,
+          positioning: SnapPositioning.relativeToAvailableSpace,
+        ),
+        color: Theme.of(context).cardColor,
+        builder: (c, state) => Container(height: 500, child: widget.child),
+        body: widget.body,
+        headerBuilder: (context, state) {
           return Container(
+            width: 30,
+            height: 5,
+            margin: const EdgeInsets.only(top: 10, bottom: 10),
             decoration: ShapeDecoration(
-                shadows: Theme.of(context).brightness == Brightness.light
-                    ? [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: const Offset(0, 3),
-                        )
-                      ]
-                    : null,
-                color: Theme.of(context).canvasColor,
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(15),
-                  topLeft: Radius.circular(15),
-                ))),
-            child: SingleChildScrollView(
-              controller: scrollController,
-              child: SafeArea(child: Container(height: 310, child: child)),
-            ),
+                color: Theme.of(context)
+                    .textTheme
+                    .bodyText1
+                    .color
+                    .withOpacity(0.2),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15))),
           );
-        }
-        // ),
-        );
+        });
   }
 }
