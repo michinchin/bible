0a4eef019ef15d30f454590395b7d134e8267128
ronjb <ron@tecarta.com>
2020-08-14 10:01:46 -0600

    Fixed bug where if opening new Bible view from Note view app would crash because call to BibleChapterState.fromJson(state.data) in window_manager.dart was returning instance with invalid values.

--- lib/models/bible_chapter_state.dart
+++ lib/models/bible_chapter_state.dart
@@ -1,20 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:tec_volumes/tec_volumes.dart';
 import 'package:tec_util/tec_util.dart' as tec;
 
+@immutable
 class BibleChapterState {
   final int bibleId;
   final BookChapterVerse bcv;
   final int page;
-  String title;
+  final String title;
 
-  BibleChapterState(this.bibleId, this.bcv, this.page, {String title}) {
-    if (title == null) {
-      final bible = VolumesRepository.shared.bibleWithId(bibleId);
-      this.title = bible.titleWithHref('${bcv.book}/${bcv.chapter}');
-    } else {
-      this.title = title;
-    }
-  }
+  BibleChapterState(this.bibleId, this.bcv, this.page, {String title})
+      : title = title ??
+            VolumesRepository.shared
+                .bibleWithId(bibleId)
+                .titleWithHref('${bcv.book}/${bcv.chapter}');
 
   static BookChapterVerse initialBCV() {
     return const BookChapterVerse(50, 1, 1);
@@ -30,9 +29,9 @@ class BibleChapterState {
 
     final json = (o is String) ? tec.parseJsonSync(o) : o;
     if (json is Map<String, dynamic>) {
-      bibleId = tec.as<int>(json['vid']);
-      bcv = BookChapterVerse.fromJson(json['bcv']);
-      page = tec.as<int>(json['page']);
+      bibleId = tec.as<int>(json['vid']) ?? 50;
+      bcv = BookChapterVerse.fromJson(json['bcv']) ?? initialBCV();
+      page = tec.as<int>(json['page']) ?? 0;
     }
 
     return BibleChapterState(bibleId, bcv, page);
