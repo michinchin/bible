2b47d3d9a7c0be4d8725aa36bb4a20b7ef2fbd00
Mike Allen <mike@tecarta.com>
2020-08-17 17:39:48 -0700

    update nav bar height computation on Android

--- android/app/src/main/kotlin/com/tecarta/TecartaBible/MainActivity.kt
+++ android/app/src/main/kotlin/com/tecarta/TecartaBible/MainActivity.kt
@@ -1,37 +1,24 @@
 package com.tecarta.TecartaBible
 
 import android.os.Bundle
-import android.view.View
-import android.view.WindowInsets
 import android.view.WindowManager
-import androidx.core.view.ViewCompat.requestApplyInsets
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    var navBarHeight = -1f;
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        window.decorView.setOnApplyWindowInsetsListener { view, insets ->
-            if (navBarHeight < 0) {
-                val density = resources.displayMetrics.density
-                navBarHeight = insets.systemWindowInsetBottom / density
-
-                if (navBarHeight == 16f) {
-                    window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-                }
-            }
-
-            insets
-        };
-    }
-
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
-        if (navBarHeight < 0) {
-            window.decorView.requestApplyInsets();
+
+        val resourceId: Int = resources.getIdentifier("navigation_bar_height",
+                "dimen", "android")
+
+        if (resourceId > 0) {
+            val density = resources.displayMetrics.density
+            val navBarHeight = resources.getDimensionPixelSize(resourceId) / density
+
+            if (navBarHeight == 16f) {
+                window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                        WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+            }
         }
     }
 }
